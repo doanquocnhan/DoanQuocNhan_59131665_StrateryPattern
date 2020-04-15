f944fe71c798433763e9873b65fb24f87fad4fcb
doanquocnhan <58899942+doanquocnhan@users.noreply.github.com>
2020-04-15 19:18:51 +0700

    v2.0

---
+++ src/BaiTap2/HangHoa.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaiTap2;
+
+/**
+ *
+ * @author Nhan
+ */
+public class HangHoa {
+    public String tenHH;
+    public int gia;
+    public String mota;
+
+    public HangHoa() {
+    }
+
+    public HangHoa(String tenHH, int gia, String mota) {
+        this.tenHH = tenHH;
+        this.gia = gia;
+        this.mota = mota;
+    }
+
+    public String getTenHH() {
+        return tenHH;
+    }
+
+    public void setTenHH(String tenHH) {
+        this.tenHH = tenHH;
+    }
+
+    public int getGia() {
+        return gia;
+    }
+
+    public void setGia(int gia) {
+        this.gia = gia;
+    }
+
+    public String getMota() {
+        return mota;
+    }
+
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+    
+    
+}
