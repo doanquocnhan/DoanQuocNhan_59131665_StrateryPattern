58b878bcba40a4ae2ca7aac92f0ba419094e6928
doanquocnhan <58899942+doanquocnhan@users.noreply.github.com>
2020-04-17 17:08:20 +0700

    v2.0

--- src/BaiTap2/GioHang.java
+++ src/BaiTap2/GioHang.java
@@ -14,11 +14,40 @@ import java.util.ArrayList;
 public class GioHang {
     public IThanhToan hinhThucTT;
     ArrayList <HangHoa> dsHH = new ArrayList();
-    
+    // Thêm hàng hóa vào danh sách
     public void themHH (HangHoa HH)
     {
         dsHH.add(HH);
     }
-    
-    
+    // chọn Hinh thức thanh toán
+    public void HTThanhtoan( IThanhToan HHThanhToan)
+    {
+           this.hinhThucTT = HHThanhToan;
+    }
+    //thanhtoan
+    public double thanhtoan(int tienhang)
+    {
+        return hinhThucTT.thanhtoan(tienhang);
+    }
+    //tính tiền
+    public int tongTienHang()
+    {
+        int tong = 0;
+        for (int i=0; i<dsHH.size(); i++){
+            tong += dsHH.get(i).getGia();
+        }
+        return tong;
+    }
+    // tổng tiền trả
+      public double tongTienGiam()
+    {
+        return hinhThucTT.thanhtoan(tongTienHang());
+    }
+    // hiển thị
+      public void hienThiGH()
+    {
+        for (int i=0; i<dsHH.size(); i++){
+            dsHH.get(i).hienThiHH();
+        }
+    }
 }
