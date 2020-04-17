64cc10b66238014c917891a1382842fe47e253fc
doanquocnhan <58899942+doanquocnhan@users.noreply.github.com>
2020-04-17 21:15:34 +0700

    v2.3

---
+++ src/BaiTap3/SinhVien.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaiTap3;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Nhan
+ */
+public class SinhVien {
+    public String Hoten;
+    public Date NgaySinh;
+    public float DiemTB;
+
+    public SinhVien(String Hoten, Date NgaySinh, float DiemTB) {
+        this.Hoten = Hoten;
+        this.NgaySinh = NgaySinh;
+        this.DiemTB = DiemTB;
+    }
+
+    public SinhVien() {
+    }
+
+    public String getHoten() {
+        return Hoten;
+    }
+
+    public void setHoten(String Hoten) {
+        this.Hoten = Hoten;
+    }
+
+    public Date getNgaySinh() {
+        return NgaySinh;
+    }
+
+    public void setNgaySinh(Date NgaySinh) {
+        this.NgaySinh = NgaySinh;
+    }
+
+    public float getDiemTB() {
+        return DiemTB;
+    }
+
+    public void setDiemTB(float DiemTB) {
+        this.DiemTB = DiemTB;
+    }
+
+    public String Showinfo() {
+        return "SinhVien{" + "Hoten=" + Hoten + ", NgaySinh=" + NgaySinh + ", DiemTB=" + DiemTB + '}';
+    }
+
+    
+    
+    
+    
+    
+}
+
